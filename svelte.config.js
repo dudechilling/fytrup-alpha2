090e36aa3052fd499d326a93de2f1ab2958054e9
dudechilling <earl@earlhealy.com>
2025-11-19 21:52:11 -0800

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,12 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
+  preprocess: preprocess(),
   kit: {
     adapter: adapter(),
-
-    // Enables correct asset resolution for GitHub Pages
-    paths: {
-      base: dev ? '' : '/fytrup-alpha2'
-    },
-
-    appDir: 'app',
-
-    // Pre-render everything (static site)
-    prerender: {
-      handleMissingId: 'ignore',
-      entries: ['*']
-    }
+    paths: { base: '' },
+    prerender: { entries: ['*'] }
   }
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-
-/** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
-  server: {
-    fs: {
-      // allow serving files from project root
-      allow: ['.']
-    }
-  },
-  build: {
-    outDir: 'build'
-  }
+  plugins: [sveltekit()]
 };
-
 export default config;
